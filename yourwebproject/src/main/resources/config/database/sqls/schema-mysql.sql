0456265112d0bdbe31b25cd8205e91741c54d876
mevroy <mevroy@gmail.com>
2018-10-12 22:08:11 +1100

    Update schema-mysql.sql

--- yourwebproject/src/main/resources/config/database/sqls/schema-mysql.sql
+++ yourwebproject/src/main/resources/config/database/sqls/schema-mysql.sql
@@ -79,4 +79,4 @@ INSERT INTO BATCH_JOB_EXECUTION_SEQ values(0);
 CREATE TABLE BATCH_JOB_SEQ (ID BIGINT NOT NULL) ENGINE=MYISAM;
 INSERT INTO BATCH_JOB_SEQ values(0);
 
-INSERT INTO groups (group_code, start_date, expiry_date, group_email_send, registration_email_send, group_long_name, avg_members_count) values ('ADM',now(), DATE_ADD(now(), INTERVAL 1 YEAR), b'0', b'0', 'Admin',0) ON DUPLICATE KEY UPDATE updated_at = now()
+INSERT INTO groups (group_code, start_date, expiry_date, group_email_send, registration_email_send, group_long_name, avg_members_count) values ('ADM',now(), DATE_ADD(now(), INTERVAL 1 YEAR), b'0', b'0', 'Admin',0) ON DUPLICATE KEY UPDATE updated_at = now();
